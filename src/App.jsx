7c5a6e94a26487773abb1d69ec844b5003aff055
xavierAshkar <xavierashkar@gmail.com>
2025-08-09 18:19:09 -0700

    Initial commit: React + Tailwind project setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-4xl">
+      Tailwind is working!
+    </div>
+  );
+}
